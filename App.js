a253627494ca72fec656cae3623f380e9e45a694
cesar903 <cesarreis521@gmai.com>
2024-08-09 16:43:37 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react"
+import { ScrollView, StatusBar, View, Text, Image } from "react-native"
+import { LightSensor } from "expo-sensors"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [{ illuminance }, definirIluminacao] = useState({ illuminance: 0 })
+  useEffect(function () {
+    LightSensor.addListener(definirIluminacao)
+  }, [])
+
+  function VerificarLuz(escuro, claro) {
+    return illuminance > 20 ? escuro : claro
+  }
+
+  return <ScrollView style={{ backgroundColor: VerificarLuz("#FFFBEB", "#20262E") }}>
+    <StatusBar
+      barStyle={VerificarLuz("dark-content", "light-content")}
+      backgroundColor={VerificarLuz("#FFFBEB", "#20262E")} />
+
+    <View>
+      <Text
+        style={{
+          color: VerificarLuz("#222", "#fff"),
+          fontSize: 32,
+          textAlign: "center"
+        }}> Sensores do Smartphone! </Text>
+
+      <Text
+        style={{
+          color: VerificarLuz("#222", "#fff"),
+          fontSize: 16,
+          textAlign: "center"
+        }}>
+        {illuminance}
+      </Text>
+
+      <Image
+        source={{
+          uri: VerificarLuz(
+            "https://www.portalriomaina.com/wp-content/uploads/2022/05/dia-do-sol.jpg",
+            "https://static.vecteezy.com/ti/fotos-gratis/t2/8440112-paisagem-noite-em-fprest-com-lua-cheia-e-nuvens-foto.jpg")
+        }}
+        style={{ height: 200 }} />
+
     </View>
-  );
+  </ScrollView >
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
